887f7bd7a7d99b8c118670785add6bf6fc785df6
pjc0179 <pjc0179@gmail.com>
2026-01-13 20:56:45 +0900

    feat: suspense, errorboundary처리

--- frontend/app/_source/components/ticketing/CountdownTimer.tsx
+++ frontend/app/_source/components/ticketing/CountdownTimer.tsx
@@ -29,7 +29,8 @@ export default function CountdownTimer({ timeLeft }: CountdownTimerProps) {
             key={index}
             className="bg-white/20 backdrop-blur-sm rounded-xl p-3"
           >
-            <div className="text-2xl md:text-3xl">
+            {/* csr ssr시간의 차이가 1초정도 남 => 하이드레이션 미스매칭문제 발생 => 1초의 차이때문에 화면 깜빡임이 존재할 이유는 없다 판단해서 에러 경고 무시를 넣음 */}
+            <div className="text-2xl md:text-3xl" suppressHydrationWarning>
               {String(item.value).padStart(2, "0")}
             </div>
             <div className="text-xs text-white/70 mt-1">{item.label}</div>
--- frontend/app/_source/components/ticketing/Ticketing.tsx
+++ frontend/app/_source/components/ticketing/Ticketing.tsx
@@ -1,29 +1,17 @@
-import PerformanceInfo from "./PerformanceInfo";
-import TicketingControls from "./TicketingControls";
-import { api } from "@/lib/api";
-import { ResponsePerformances } from "@/types/performance";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import TicketingData from "./TicketingData";
 
-async function getLatestPerformance() {
-  try {
-    const response = await api.get<ResponsePerformances>(
-      `/api/mock/performances?limit=1`
-    );
-    return response;
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-export default async function Ticketing() {
-  const data = await getLatestPerformance();
-  const performance = data?.performances[0];
-
+export default function Ticketing() {
   return (
     <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <div className="bg-gradient-to-br from-purple-600 via-purple-500 to-pink-500 rounded-3xl p-8 md:p-12 text-white shadow-2xl">
         <div className="grid md:grid-cols-2 gap-8 items-center">
-          <PerformanceInfo performance={performance} />
-          <TicketingControls performance={performance} />
+          <ErrorBoundary fallback={<div>추후 에러 표시 화면 구현</div>}>
+            <Suspense fallback={<div>로딩.... 필요한가??</div>}>
+              <TicketingData />
+            </Suspense>
+          </ErrorBoundary>
         </div>
       </div>
     </main>
