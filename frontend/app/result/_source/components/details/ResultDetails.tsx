a2556de324207e2114e839215e37ad923add3ee6
ParkTjgus <psh22112100@gmail.com>
2026-01-15 05:00:20 +0900

    fix: build error
    - SelectedSeats를 동적으로 임포트
    -  `ssr: false` 옵션 설정하여 빌드 시점에 서버에서 실행되지 않고 브라우저(클라이언트)에서만 실행

--- frontend/app/result/_source/components/details/ResultDetails.tsx
+++ frontend/app/result/_source/components/details/ResultDetails.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import SelectedSeats from "./SelectedSeats";
+import dynamic from "next/dynamic";
 import TimeLog from "./TimeLog";
 import UserRank from "./UserRank";
+
+const SelectedSeats = dynamic(() => import("./SelectedSeats"), {
+  ssr: false,
+  loading: () => <div>결과를 불러오는 중입니다.</div>,
+});
 
 export default function ResultDetails() {
   return (
@@ -10,7 +17,7 @@ export default function ResultDetails() {
       <ErrorBoundary
         fallback={<div>결과를 불러오는 중 오류가 발생했습니다.</div>}
       >
-        <Suspense fallback={<div>결과를 불러오는 중입니다...</div>}>
+        <Suspense fallback={<div>결과를 불러오는 중입니다.</div>}>
           <SelectedSeats />
           <TimeLog />
           <UserRank />
