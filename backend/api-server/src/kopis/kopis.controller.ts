45dde374a15185c429f7bca3ceabefe3cdbfe7ca
ParkTjgus <psh22112100@gmail.com>
2026-02-02 11:44:20 +0900

    fix: 수동 동기화 id 중복문제 수정

--- backend/api-server/src/kopis/kopis.controller.ts
+++ backend/api-server/src/kopis/kopis.controller.ts
@@ -26,9 +26,14 @@ export class KopisController {
   async syncManual(@Body() body: ManualSyncDto) {
     const startDate = body.start_date ? new Date(body.start_date) : undefined;
     const endDate = body.end_date ? new Date(body.end_date) : undefined;
+    const idIdentifier = body.id_identifier;
 
     try {
-      await this.kopisScheduler.syncPerformances(startDate, endDate);
+      await this.kopisScheduler.syncPerformances(
+        startDate,
+        endDate,
+        idIdentifier,
+      );
       return { message: 'KOPIS data sync completed' };
     } catch (error) {
       if (error instanceof Error) {
