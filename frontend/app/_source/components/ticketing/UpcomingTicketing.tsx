057b35c196aa1ad6769f5959b37b2b24bd21905c
pjc0179 <pjc0179@gmail.com>
2026-02-03 12:00:42 +0900

    feat: 메인 티켓팅에서 플랫폼 설정 가능 기능 추가

--- frontend/app/_source/components/ticketing/UpcomingTicketing.tsx
+++ frontend/app/_source/components/ticketing/UpcomingTicketing.tsx
@@ -1,11 +1,12 @@
-import { ErrorBoundary } from 'react-error-boundary';
-import { Suspense } from 'react';
-import TicketingData from './TicketingData';
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import TicketingData from "./TicketingData";
+import Links from "./links/Links";
 
 export default function UpcomingTicketing() {
   return (
     <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col gap-8">
-      <div className="bg-gradient-to-br from-purple-600 via-purple-500 to-pink-500 rounded-3xl p-8 md:p-12 text-white shadow-2xl">
+      <div className="bg-gradient-to-br from-purple-600 via-purple-500 to-pink-500 rounded-3xl px-8 md:p-12 text-white shadow-2xl">
         <div className="grid md:grid-cols-2 gap-8 items-center">
           <ErrorBoundary fallback={<div>추후 에러 표시 화면 구현</div>}>
             <Suspense fallback={<div>공연 정보를 불러오는 중입니다.</div>}>
@@ -13,6 +14,7 @@ export default function UpcomingTicketing() {
             </Suspense>
           </ErrorBoundary>
         </div>
+        <Links />
       </div>
     </main>
   );
