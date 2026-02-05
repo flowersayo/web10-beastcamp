1888ac43924d1446187a9a1e316b2ca40409127e
viixix <hmu0516@unist.ac.kr>
2026-02-05 13:57:13 +0900

    feat(ticket-scheduler): TraceModule 추가
    - TicketSchedulerModule에 TraceModule을 추가하여 트레이스 기능 통합
    - 의존성 관리 개선

--- backend/ticket-server/src/ticket-scheduler/ticket-scheduler.module.ts
+++ backend/ticket-server/src/ticket-scheduler/ticket-scheduler.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigModule } from '@nestjs/config';
+import { TraceModule } from '@beastcamp/shared-nestjs';
 import { TicketSetupModule } from '../ticket-setup/ticket-setup.module';
 import { TicketSchedulerService } from './ticket-scheduler.service';
 
 @Module({
-  imports: [TicketSetupModule, ScheduleModule.forRoot(), ConfigModule],
+  imports: [
+    TicketSetupModule,
+    ScheduleModule.forRoot(),
+    ConfigModule,
+    TraceModule,
+  ],
   providers: [TicketSchedulerService],
   exports: [TicketSchedulerService],
 })
